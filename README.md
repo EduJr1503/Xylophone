02fc6daa4d08cba0d01f326b8a69ec3dd135ae29
Eduardo Junior <94651797+EduJr1503@users.noreply.github.com>
2022-07-19 00:13:14 -0300

    Add files via upload

---
+++ README.md
@@ -0,0 +1,24 @@
+# Xylophone
+
+## App:
+
+This application was made in Swift based on the course provided by Angela Yu from The App Brewery on Udemy as a form of study.
+
+## It was learned in this app
+
+* How to play sound using AVFoundation and AVAudioPlayer.
+* Understand Apple documentation and how to use StackOverflow.
+* Functions and methods in Swift. 
+* Data types.
+* Swift loops.
+* Variable scope.
+* The ViewController lifecycle.
+* Error handling in Swift.
+* Code refactoring.
+* Basic debugging.
+
+
+>This is a companion project to The App Brewery's Complete App Development Bootcamp, check out the full course at [www.appbrewery.co](https://www.appbrewery.co/)
+
+![End Banner](Documentation/readme-end-banner.png)
+
---
+++ Xylophone/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Eduardo Junior on 19/07/2022.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print(sender.currentTitle)
+        
+        playSound(soundName: sender.currentTitle!)
+    }
+    
+    func playSound(soundName: String) {
+        let url = Bundle.main.url(forResource: soundName, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
